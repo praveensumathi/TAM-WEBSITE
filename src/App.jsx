63d1902e6667eda9398fd9b859afabd5b702bcce
dharaniya-thiyagarajan <dharaniya2000@gmail.com>
2023-06-26 14:30:48 +0530

    route remove

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
-
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Drawer from "./Drawer";
-import InfoStream from "./InfoStream";
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Drawer />} />
-          <Route path="/1" element={<InfoStream/>} />
-        </Routes>
-      </BrowserRouter>
-
-      {/* <Drawer/> */}
+      <Drawer />
     </div>
   );
 }
 
-export default App
+export default App;

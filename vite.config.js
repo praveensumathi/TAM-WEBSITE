bd6bd31957f30b3f1224e4c256ba1cea79a15f7e
Jeevanand5804 <jeevanand5804@gmail.com>
2023-07-04 15:01:59 +0530

    change port

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/TAM-WEBSITE/",
+  // plugins: [react()],
+  // base: "/TAM-WEBSITE/",
+  server: {
+    host: true,
+    port:80
+  }
 });
